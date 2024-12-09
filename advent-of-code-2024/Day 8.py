8efc7cba4194af84abe991d0f1cc166995829721
Luke Davis <lukeda117@gmail.com>
2024-12-09 17:30:24 +0000

    Day 8 Part One

--- advent-of-code-2024/Day 8.py
+++ advent-of-code-2024/Day 8.py
@@ -1,18 +1,7 @@
 import itertools
 
-def get_line(point_a, point_b):
-    # Points in form (x,y)
-    # Given two points get a line that both points lie on
-    # y = mx +c
-    # return form this the gradient, m
-    # return m as tuple (change in y, change in x)
 
-    m = (point_b[1]-point_a[1],point_b[0]-point_a[0])
-    # c = point_a[1] - (m * point_a[0])
-
-    return m
-
-def get_all(frequency, map):
+def get_all_points(frequency, map):
     # Returns a list of tuples
     # Each tuple is a point (row_index, col_index) for each occurence of that frequency
     occurences = []
@@ -21,7 +10,6 @@ def get_all(frequency, map):
         for col_index in range(len(map[row_index])):
             point = map[row_index][col_index]
             if point == frequency:
-                print(row_index)
                 occurences.append((row_index, col_index))
 
     return occurences
@@ -31,56 +19,46 @@ def get_antinodes(input_map):
     # Use a string to keep track of nodes which have been looked at
     different_freq = ""
     grid = input_map.split("\n")
+    # Create an antinode grid to keep track of where antinodes are - as need to know UNIQUE locations
+    antinode_grid = ["." * len(row) for row in grid]
 
+    # Run through grid and get any different frequencies
     for row_index in range(len(grid)):
         for col_index in range(len(grid[row_index])):
             point = grid[row_index][col_index]
             if (point.isalpha() or point.isdigit()) and point not in different_freq:
-                different_freq += point
+                frequency = point
+                # Approach to find different antinodes is to add or subtract the change in y and change in x between the two antenna
+                antenna_points = get_all_points(frequency, grid)
+                # Get all possible pairs of antenna points for this frequency
+                frequency_pairs = [p for p in itertools.product(antenna_points, repeat=2)]
+                # For each pair - (if pair not equal) - get line between this pair
+                # Plot a # where the node would be
+                for pair in frequency_pairs:
+                    if pair[0] != pair[1]:
+                        # Each pair appears twice in frequency pairs list but in reverse order
+                        # So simply add the difference between the two antenna in the pair
+                        difference = (pair[0][0] - pair[1][0], pair[0][1] - pair[1][1])
+                        # Add difference to first point that appears in pair (doesn't really matter as pair appears twice)
+                        antinode_row_index = pair[0][0] + difference[0]
+                        antinode_col_index = pair[0][1] + difference[1]
+                        # Check if new pos to add in grid -if is add it
+                        if 0 <= antinode_row_index < len(grid) and 0 <= antinode_col_index < len(grid[0]):
+                            # To make more readable insert into string in seperate parts
+                            before = antinode_grid[antinode_row_index][:antinode_col_index]
+                            after = antinode_grid[antinode_row_index][antinode_col_index+1:]
+                            antinode_grid[antinode_row_index] = before + "#" + after
 
-    print(different_freq)
-    for frequency in different_freq:
-        antenna_points = get_all(frequency, grid)
-        # Get all possible pairs of antenna points for this frequency
-        frequency_pairs = [p for p in itertools.product(antenna_points,repeat=2)]
-        # For each pair - (if pair not equal) - get line between this pair
-        # Plot a # where the node would be
-        for pair in frequency_pairs:
-            if pair[0] != pair[1]:
-                # Row index is y
-                m = get_line(pair[0], pair[1])
-                # Node is two m (the gradient) away from each pair
-                # - gradient for lower pair
-                # + gradient for higher pair
-                # Must check if node index exists
-                # Need to do element wise tuple addition
-                node0_pos = tuple(map(lambda i, j: i - j, pair[0], 2*m))
-                node1_pos = tuple(map(lambda i, j: i + j, pair[1], 2 * m))
+    # Get total
+    total = 0
+    for row in antinode_grid:
+        for point in row:
+            if point == "#":
+                total += 1
 
-                print(pair)
-                print(m)
-                print(node0_pos)
-
-
-
-
-
-
-
-
+    return total
 
 
+puzzle_input = """"""
 
-
-puzzle_input = """..........
-...#......
-..........
-....a.....
-..........
-.....a....
-..........
-......#...
-..........
-.........."""
-
-get_antinodes(puzzle_input)
+print(get_antinodes(puzzle_input))
