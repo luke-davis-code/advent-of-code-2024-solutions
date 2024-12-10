afcf5d111f07869389d5f3f3cb6f2f7818ae8045
Luke Davis <lukeda117@gmail.com>
2024-12-10 12:43:05 +0000

    Day 9 Part One (Using list not string)

--- advent-of-code-2024/Day 9.py
+++ advent-of-code-2024/Day 9.py
@@ -1,7 +1,9 @@
+# PART ONE
+
 def decode_format(diskmap):
     # Converts a dense format diskmap into a string representation of the disk with . representing free space
     # Also returns the maximum id number for this disk as needed when compacting
-    disk = ""
+    disk = []
 
     # Use a bool to know if looking at length of file or length of free space
     free_space = False
@@ -12,76 +14,49 @@ def decode_format(diskmap):
 
     while i < len(diskmap):
         if not free_space:
-            disk += str(id) * int(diskmap[i])
+            for j in range(int(diskmap[i])):
+                disk.append(str(id))
             id += 1
         else:
-            disk += "." * int(diskmap[i])
+            for j in range(int(diskmap[i])):
+                disk.append(".")
         i += 1
         free_space = not free_space
 
-    return disk, id-1
+    return disk
 
-def compact(disk, max_id):
-    # Take in string representation of disk (and max id number) and compact it using the amphipod's process
-    compact_disk = disk
-    print(disk)
-    print()
+
+
+def compact(disk):
+    # Take in list representation of disk and compact it using the amphipod's process
+    compact_disk = disk.copy()
 
     # Move file blocks one at a time (from the end) until no gaps between fileblocks
-    # Also keep track of size of fileblocks being looked at using num digits
-    current_id = max_id
-    block_size = len(str(current_id))
-    i = len(compact_disk) - block_size
+
+    # Have a pointer i - points to current fileblock being looked at (from the right)
+    i = len(disk)
+
     while compact_disk[:i].count(".") > 0:
-        # Get index of first free space from the left that can fit current block_size
-        # .index() finds first occurence of item
+        i -= 1
+
         free_space_index = compact_disk.index(".")
-        print()
-        print(len(compact_disk) - i)
-        print()
+        file_block = compact_disk[i]
 
-        file_block = compact_disk[i:i + block_size]
-        free_space = compact_disk[free_space_index]
-        print(block_size)
-        print(file_block)
-        print(free_space)
-        print()
-        # Swap free space with file block at end of disk
-        before_space = compact_disk[:free_space_index]
-        between = compact_disk[free_space_index+1:i]
-        after_file_block = compact_disk[i + block_size:]
-        compact_disk = before_space + file_block + between + "."
+        # Now swap these
+        compact_disk[free_space_index] = file_block
+        compact_disk[i] = "."
 
-        # Now change file block size if next file block looking at is different
-        next_file_block = compact_disk[i:i + block_size]
-        if next_file_block != file_block:
-            current_id -= 1
-            block_size = len(str(current_id))
-
-        i -= block_size
-
-        print()
-        print(compact_disk)
-        print()
     return compact_disk
 
+def checksum(filesystem):
+    total = 0
+    # Take in a filesystem as a list and return its checksum
+    for position in range(len(filesystem)):
+        if filesystem[position] != ".":
+            total += position * int(filesystem[position])
 
-def checksum(disk):
-    # Take in string representation of a disk and return its checksum
-    sum = 0
+    return total
 
-    # Use a count to account for ids being more than one digit
-    count = 0
+puzzle_input = ""
 
-    for position in range(0, len(disk)):
-        id = disk[position]
-        if id != ".":
-            sum += position * int(id)
-
-    return sum
-
-
-puzzle_input = "156769588015262926819037393"
-
-disk, id = decode_format(puzzle_input)
-print(checksum(compact(disk, id)))
+print(checksum(compact(decode_format(puzzle_input))))
