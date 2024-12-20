5be359529ea858f8b044294da2a244c14a632ddc
Luke Davis <lukeda117@gmail.com>
2024-12-20 19:43:38 +0000

    Day 2 Part Two

--- advent-of-code-2024/Day 1.py
+++ advent-of-code-2024/Day 1.py
@@ -16,26 +16,55 @@ def get_smallest(list):
 
 puzzle_input = """"""
 
-list1 = []
-list2 = []
+leftlist = []
+rightlist = []
 for line in puzzle_input.split("\n"):
-    list1.append(int(line.split("   ")[0]))
-    list2.append(int(line.split("   ")[1]))
+    leftlist.append(int(line.split("   ")[0]))
+    rightlist.append(int(line.split("   ")[1]))
+
+# Get total difference score
 
 total = 0
 count = 0
 
-while count < len(list1):
-    index1 = get_smallest(list1)
-    index2 = get_smallest(list2)
-    num1 = list1[index1]
-    num2 = list2[index2]
+while count < len(leftlist):
+    index1 = get_smallest(leftlist)
+    index2 = get_smallest(rightlist)
+    num1 = leftlist[index1]
+    num2 = rightlist[index2]
     total += abs(num1 - num2)
-    list1[index1] = -1
-    list2[index2] = -1
+    leftlist[index1] = -1
+    rightlist[index2] = -1
     count += 1
 
 print(total)
+
+
+# PART TWO
+
+# Reset lists
+leftlist = []
+rightlist = []
+for line in puzzle_input.split("\n"):
+    leftlist.append(int(line.split("   ")[0]))
+    rightlist.append(int(line.split("   ")[1]))
+
+def get_similarity(leftnum, rightlist):
+    count = 0
+    for rightnum in rightlist:
+        if leftnum == rightnum:
+            count += 1
+    return leftnum * count
+
+# Get total similarity score
+total = 0
+for num in leftlist:
+    total += get_similarity(num, rightlist)
+
+print(total)
+
+
+
 
 
 
